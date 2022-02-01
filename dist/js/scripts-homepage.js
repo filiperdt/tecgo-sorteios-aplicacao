35dd644b276522de4dc01c670fb8aef2116b1808
Rúben Filipe Andrade de Oliveira <rubenfilipeao@gmail.com>
2022-02-01 11:08:49 -0300

    - Criada a página Admin (visual); - Criada a página Login (visual); - Criada a página Resete Senha (visual); - Criada a página Cadastro (visual); - Reestruturadas as pastas do projeto; - Alterados os nomes de arquivos; - Alterados os paths de arquivos; - Passados styles e scripts para arquivos externos.

--- dist/js/scripts-homepage.js
+++ dist/js/scripts-homepage.js
@@ -3,5 +3,4 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
---
+++ src/js/scripts-item.js
@@ -0,0 +1,29 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip({html:true})
+})
+
+function copyToClipboard(pChavePixCopiada) {
+    var copyText = document.getElementById("chavePix");
+    var textArea = document.createElement("textarea");
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    textArea.remove();
+
+    var pChavePixCopiada = document.getElementById(pChavePixCopiada);
+    pChavePixCopiada.textContent = 'Copiado!';
+}
+
+function changePChavePixCopiada(pChavePixCopiada) {
+    var pChavePixCopiada = document.getElementById(pChavePixCopiada);
+    pChavePixCopiada.textContent = 'Clique para copiar';
+}
+
+function enviarComprovante() {
+    var telefone = "553284915355";
+    var mensagem = "Te enviarei o comprovante do pagamento da rifa 'Carros envelopados'";
+    
+    var link = 'https://wa.me/' + telefone + '?text=' + mensagem;
+    window.open(link);
+}
