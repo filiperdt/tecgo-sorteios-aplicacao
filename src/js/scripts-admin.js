d60e828ac05bb7917a16413e4ca30538ba850f59
Rúben Filipe Andrade de Oliveira <rubenfilipeao@gmail.com>
2022-02-02 22:43:17 -0300

    - Inseridos os botões de Exibir, Editar, e Excluir na tabela de rifas; - Alterado o estilo da tabela de rifas; - Excluídos arquivos desnecessários do projeto; - Criada a página dinâmica para criar rifa; - Criada a página dinâmica para listar rifas; - Criada a página dinâmica para exibir rifa; - Criada a página dinâmica para atualizar rifa.

--- src/js/scripts-admin.js
+++ src/js/scripts-admin.js
@@ -2,9 +2,9 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip({html:true})
 })
 
-// Traduz o DataTable
+// Inicializa e traduz o DataTable
 $(document).ready(function() {
-    $('#datatablesSimple').DataTable( {
+    $('.datatablesSimple').DataTable( {
         "language": {
             "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
         }
